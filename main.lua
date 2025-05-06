c393961d5e6639beb98f4fe3da1f5252da30dbf4
n00dlephr0g <66238100+n00dlephr0g@users.noreply.github.com>
2025-05-06 05:41:11 +0000

    cool did some stuff

--- main.lua
+++ main.lua
@@ -4,71 +4,53 @@ This is a debug plugin to test Plugin functionality.
 @module koplugin.HelloWorld
 --]]--
 
--- This is a debug plugin, remove the following if block to enable it
+
 
 local Dispatcher = require("dispatcher")  -- luacheck:ignore
+
+-- widgets
 local InfoMessage = require("ui/widget/infomessage")
+local MultiInput = require("ui/widget/multiinputdialogue")
 local UIManager = require("ui/uimanager")
 local WidgetContainer = require("ui/widget/container/widgetcontainer")
 local _ = require("gettext")
-local settings = require("settings")
+
+
+-- works
 local koplanner = WidgetContainer:extend{
     name = "koplanner",
     is_doc_only = false,
 }
 
+-- works
 function koplanner:onDispatcherRegisterActions()
     Dispatcher:registerAction("koplanner_ui_show", {category="KOPlanner", event="KoplannerUiShow", title=_("Show KOPlanner UI"), general=true,})
 end
 
+-- works
 function koplanner:init()
     self:onDispatcherRegisterActions()
     self.ui.menu:registerToMainMenu(self)
 end
 
+
 function koplanner:addToMainMenu(menu_items)
-    menu_items.hello_world = {
+    menu_items.koplanner = {
         text = _("KOPlanner"),
         -- in which menu this should be appended
         sorting_hint = "tools",
         -- a callback when tapping
         sub_item_table = {
             {
-                text = _("test"),
+                text = _("Server Settings"),
                 keep_menu_open = true,
+                
                 callback = function()
-                    UIManager:show(settings:test())
+                   UIManager:show() 
                 end
             },
             {
-                text = _("Wireless Settings"),
-                keep_menu_open = true,
-                callback = function()
-                    UIManager:show(InfoMessage:new{
-                        text = _("showing calendar window")
-                    })
-                end
-            },
-            {
-                text = _("Calendar Settings"),
-                keep_menu_open = true,
-                callback = function()
-                    UIManager:show(InfoMessage:new{
-                        text = _("showing calendar window")
-                    })
-                end
-            },
-            {
-                text = _("Sync Mode"),
-                keep_menu_open = true,
-                callback = function()
-                    UIManager:show(InfoMessage:new{
-                        text = _("showing sync mode selection")
-                    })
-                end
-            },
-            {
-                text = _("Sync Settings"),
+                text = _("Sync Settings (TODO)"),
                 keep_menu_open = true,
                 callback = function()
                     UIManager:show(InfoMessage:new{
